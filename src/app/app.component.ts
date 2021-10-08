6d7312ac5e6a0edee0ffbc6d934022bc3c8f223a
Dmitry Khilko <50961714+KhilkoDmitryCDR@users.noreply.github.com>
2021-10-08 10:36:13 +0200

    create observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable, Subject, Subscription } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,36 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'UsersApp';
   userId=15;
+  message:any;
+  subscription!:Subscription;
+  
+ 
+
+  source = new Observable(function (observer) {
+    setTimeout(function () {
+      console.log('Timeout');
+      observer.next(100);
+      observer.complete();
+    }, 1500);
+    console.log('started');
+  });
+  sub=this.source.subscribe(
+    function(value){
+      console.log("next"+value);
+    },
+    function(error){
+      console.error(error);
+
+    },
+    function(){
+      console.info("complited");
+
+    }
+    );
+
+
+   
+
+
+  
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { observable, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -8,10 +9,28 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserComponent implements OnInit {
 
+ 
+
   constructor(private router:ActivatedRoute) {
     this.router.params.subscribe(params=>console.log(params))
    }
-  ngOnInit(): void {
-  }
 
-}
+  
+  ngOnInit(): void {
+    console.clear();
+
+  }
+   source=new Observable(function(observer){
+     setTimeout(function(){
+       console.log("Timeout");
+       observer.next(100);
+       observer.complete();
+     },1500
+     );
+     console.log("started")
+   }
+   );
+
+  
+
+   }
